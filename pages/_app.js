5988c6ec42acdb6e9ba9f80b99fa588083afc847
chriscervantes2415 <christianjeffersoncervantes@gmail.com>
2024-02-08 16:21:10 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react"
+import {  Poppins } from "next/font/google";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const inter = Poppins({ subsets: ["latin"], weight: '400'});
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  return <main className={'${inter.className}'}>
+    <SessionProvider session={session}>
+      <Header/>
+    <Component {...pageProps} />;
+    </SessionProvider>
+  </main>
 }
